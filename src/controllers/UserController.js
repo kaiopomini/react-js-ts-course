0b7426f411dac2e38407e1a69e850cf3e9744a74
Kaio Pomini <kaiopomini@gmail.com>
2021-06-06 11:01:46 -0300

    users crud

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,11 +4,84 @@ class UserController {
   async store(req, res) {
     try {
       const newUser = await User.create(req.body);
-      res.json(newUser);
+      return res.json(newUser);
     } catch (error) {
-      res.status(400).json({
+      return res.status(400).json({
         errors: error.errors.map((err) => err.message),
       });
+    }
+  }
+
+  // index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // show
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // update
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      const updatedUser = await user.update(req.body);
+      return res.json(updatedUser);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // delete
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['ID não enviado'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(null);
+    } catch (error) {
+      return res.status(400).json(null);
     }
   }
 }
